4f3fe0e67a69759e9bab9af805ec8e2a83253639
AL <powerbuoy@gmail.com>
2016-07-27 01:39:21 +0200

    DontScroll

---
+++ dont-scroll.js
@@ -0,0 +1,26 @@
+(function (factory) {
+	'use strict';
+
+	if (typeof define === 'function' && define.amd) {
+		define(['jquery'], factory);
+	}
+	else if (typeof module === 'object' && typeof module.exports === 'object') {
+		module.exports = factory(require('jquery'));
+	}
+	else {
+		factory(jQuery);
+	}
+}(function ($) {
+	'use strict';
+
+	$.fn.dontScroll = function () {
+		return this.click(function (e) {
+			e.preventDefault();
+
+			var st = $(document).scrollTop();
+			window.location.hash = $(this).attr('href');
+
+			$(document).scrollTop(st);
+		});
+	};
+});
